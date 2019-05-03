309efb7d121639a077bce4d3aa5fce60b058c4c6
unknown <amberj.kolar@gmail.com>
2019-05-03 14:10:48 -0700

    Added new information & changed website format to include multiple pages

---
+++ index.js
@@ -0,0 +1,31 @@
+var imgCarousel = {
+  images: ["assets/images/faceShot.jpg", "assets/images/ncwit1.jpg", "assets/images/ncwit2.jpg"],
+  currentImg: 0
+};
+setInterval(function() {
+  ++imgCarousel.currentImg;
+  if (imgCarousel.currentImg > 2) {
+    imgCarousel.currentImg = 0;
+  }
+  document.getElementById("imgCarousel").src = imgCarousel.images[imgCarousel.currentImg];
+}, 6000);
+
+function toggleNavigation() {
+	if (document.getElementById("navigation").style.display == "none") {
+		document.getElementById("navigation").style.display = "inline-block";
+	} else {
+		document.getElementById("navigation").style.display = "none";
+	}
+}
+toggleNavigation();
+function navDrop(id) {
+	document.getElementById(id).style.visibility = "visible";
+}
+function navLift(id) {
+	document.getElementById(id).style.visibility = "hidden";
+}
+navLift("list1");
+navLift("list2");
+navLift("list3");
+navLift("list4");
+navLift("list5");
